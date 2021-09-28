582b927ccf753613529fdce01781e2dd9add43e0
Kevin-DA-RE <74685387+Kevin-DA-RE@users.noreply.github.com>
2021-09-28 19:57:55 +0200

    Components Electroménager

--- Angular/myApp/src/app/appareil/appareil.component.ts
+++ Angular/myApp/src/app/appareil/appareil.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-appareil',
   templateUrl: './appareil.component.html',
   styleUrls: ['./appareil.component.scss']
 })
+
 export class AppareilComponent implements OnInit {
+
+  @Input()
+  appareilName!: string;
+
+  @Input()
+  appareilStatus: string= 'éteint';
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  getStatus(){
+    return this.appareilStatus;
   }
 
 }
