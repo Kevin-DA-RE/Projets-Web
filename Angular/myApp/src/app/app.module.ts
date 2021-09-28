5eb9e9fbc3d2c3c30630430a12748ef6f6c1b30f
Kevin-DA-RE <74685387+Kevin-DA-RE@users.noreply.github.com>
2021-09-28 16:49:42 +0200

    first appli

--- Angular/myApp/src/app/app.module.ts
+++ Angular/myApp/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FirstComponentComponent } from './first-component/first-component.component';
+import { AppareilComponent } from './appareil/appareil.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FirstComponentComponent,
+    AppareilComponent
   ],
   imports: [
     BrowserModule
