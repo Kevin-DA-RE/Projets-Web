74af8f97f2ac959e449c1ad1a875b08295a02acd
Kevin-DA-RE <74685387+Kevin-DA-RE@users.noreply.github.com>
2021-10-07 09:41:01 +0200

    Zone de test object

---
+++ Js-Divers/Js - test objet/object.js
@@ -0,0 +1,22 @@
+car = [
+    {
+        model: 'Peugeot',
+        mark: 307
+    },
+    {
+        model: 'Renault',
+        mark: 'Megane'
+    },
+    {
+        model: 'Citroen',
+        mark: 'Picasso'
+    }
+];
+
+let leightCar = car.length;
+let car1 = car[0];
+let modelCar1 = car[0].model;
+
+console.log(leightCar);
+console.log(car1);
+console.log(modelCar1);
